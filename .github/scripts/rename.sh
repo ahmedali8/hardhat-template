d16ad81a8f313ea49befd13388ebe5a2a71f30ba
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-09-10 12:18:05 +0500

    chore(n): update readme on repo creation
    update badges in readme file with the newly created repo and gitpod option
    
    resolves #136

--- .github/scripts/rename.sh
+++ .github/scripts/rename.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REPOSITORY=${1?Error: Pass username/repo e.g. @prb/hardhat-template}
+REPOSITORY=${1?Error: Pass username/repo e.g. prb/hardhat-template}
 REPOSITORY_OWNER=${2?Error: Pass username e.g. prb}
 
 echo "REPOSITORY: $REPOSITORY"
@@ -8,7 +8,7 @@ echo "REPOSITORY_OWNER: $REPOSITORY_OWNER"
 
 # jq is like sed for JSON data
 JQ_OUTPUT=`jq \
-  --arg NAME "$REPOSITORY" \
+  --arg NAME "@$REPOSITORY" \
   --arg AUTHOR_NAME "$REPOSITORY_OWNER" \
   --arg URL "https://github.com/$REPOSITORY_OWNER" \
   '.name = $NAME | .description = "" | .author |= ( .name = $AUTHOR_NAME | .url = $URL )' \
@@ -16,3 +16,12 @@ JQ_OUTPUT=`jq \
 
 # overwrite package.json
 echo "$JQ_OUTPUT" > package.json
+
+# Make sed command compatible in both Mac and Linux environments
+# Reference: https://stackoverflow.com/a/38595160/8696958
+sedi () {
+  sed --version >/dev/null 2>&1 && sed -i -- "$@" || sed -i "" "$@"
+}
+
+# Rename instances of "paulrberg/hardhat-template" to the new repo name in README.md for badges only
+sedi -e '/Use this template/! s|paulrberg/hardhat-template|'${REPOSITORY}'|;' 'README.md'
