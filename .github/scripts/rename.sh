71bcad8823111132af49d3a7c6f43529b0013533
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-09-18 16:50:40 +0500

    chore: testing create

--- .github/scripts/rename.sh
+++ .github/scripts/rename.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# Make sed command compatible in both Mac and Linux environments
-# Reference: https://stackoverflow.com/a/38595160/8696958
-sedi () {
-  sed --version >/dev/null 2>&1 && sed -i -- "$@" || sed -i "" "$@"
-}
-
 # https://gist.github.com/vncsna/64825d5609c146e80de8b1fd623011ca
 set -euo pipefail
 
@@ -29,4 +23,4 @@ JQ_OUTPUT=`jq \
 echo "$JQ_OUTPUT" > package.json
 
 # Rename instances of "paulrberg/hardhat-template" to the new repo name in README.md for badges only
-sedi "/Use this template/! s|paulrberg/hardhat-template|'${REPOSITORY}'|;" "README.md"
+sed -i -e "/Use this template/! s|paulrberg/hardhat-template|'${REPOSITORY}'|;" "README.md"
